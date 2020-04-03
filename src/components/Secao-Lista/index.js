fb8ada520aca241f32cad063379e299651bb0fe5
Felipe <felipe.lucena@iteris.com.br>
2020-04-02 23:53:26 -0300

    Troca de nome SecaoList para SecaoLista, pradronizando pt-BR

--- src/components/Secao-Lista/index.js
+++ src/components/Secao-Lista/index.js
@@ -3,21 +3,15 @@ import React, { Component } from 'react';
 import './styles.css';
 
 export default class SecaoLista extends Component {
-  renderData = () => {
-    return this.props.list.map( list =>
-      <li>{list}</li>
-      )
-  }
 	
 	render() {
-		
 		return (
 			<section className="secao">
         <div>
           <img src={this.props.src} className="rounded-img" alt={this.props.alt} title={this.props.title}></img>
           <h3>{this.props.title}</h3>
           <ul>
-            { this.renderData(this.props.list) }
+            {this.props.list.map(item => <li key={item}>{item}</li>)}
           </ul>
         </div>
       </section>
