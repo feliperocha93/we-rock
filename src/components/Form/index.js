155b8da7786ae802df8a340b481d4a4ac16dccb2
Felipe <felipe.lucena@iteris.com.br>
2020-04-10 21:19:50 -0300

    Implementando componente formulário

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,20 +2,59 @@ import React from 'react';
 
 import './styles.css';
 
-import github from '../../assets/img/icons/github-icon.png';
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
-const Form = () => (
-	<form method="post" name="form" className="formphp grid-10">
-    <label for="nome">Nome</label>
-    <input id="nome" name="nome" type="text" className="form_contato_campo">
-    <label for="email">E-mail</label>
-    <input id="email" name="email" type="text" className="form_contato_campo">
-    <label for="telefone">Telefone</label>
-    <input id="telefone" name="telefone" type="text" className="form_contato_campo">
-    <label for="mensagem">Mensagem</label>
-    <textarea name="mensagem" id="mensagem" className="form_contato_campo"></textarea>
-    <button id="enviar" name="enviar" type="submit" className="">Enviar</button>
-  </form>
-);
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
+}
 
 export default Form;
