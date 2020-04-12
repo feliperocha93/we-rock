578b9ba4bdececb68958e6d296bb5921515a75d4
Felipe <felipe.lucena@iteris.com.br>
2020-04-12 15:40:00 -0300

    Integração do formulário

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,60 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
-
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
-
-class Form extends React.Component {
+export default class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", email: "", message: "" };
+    this.state = {};
   }
 
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Enviado");
+  };
 
   render() {
-    const { name, email, message } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" value={message} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <div>
+        <h1>Contact</h1>
+        <form name="contact" method="post" onSubmit={this.handleSubmit}>
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
     );
   }
 }
-
-export default Form;
