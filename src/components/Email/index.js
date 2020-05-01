d548e974506e67996bac13ded041c33564ef7d4a
Felipe <fee.rocha@hotmail.com>
2020-05-01 20:20:48 -0300

    Salvando e-mail na API

--- src/components/Email/index.js
+++ src/components/Email/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 import { MdSend } from 'react-icons/md';
 
 import './styles.css';
@@ -10,7 +11,13 @@ export default class Email extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.email);
+    const email = this.state.email;
+    api.get('email').then(response => {
+      const id = response.data.length + 1;
+      const request = { id, email: this.state.email }
+      console.log(request);
+      api.post('email', request);
+    });
   }
 
   handleChange = e => {
@@ -24,9 +31,9 @@ export default class Email extends Component {
           <h3>Fique ligado no Blog!</h3>
           <span>Deixe seu e-mail para receber um alerta quando rolar novidade aqui no Blog</span>
           <form className="input-bar" onSubmit={this.handleSubmit}>
-            <input placeholder="Sem span. Pode confiar." type="text" onChange={this.handleChange} />
+            <input placeholder="Sem span. Pode confiar." type="email" onChange={this.handleChange} />
             <button type="submit">
-              <MdSend size={30} color="#4cdef5" className="hover-scale" />
+              <MdSend size={30} color="#999" className="hover-scale" />
             </button>
           </form>
         </div>
